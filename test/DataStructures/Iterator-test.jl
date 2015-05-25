6b7bdf1931550450beae1b924eb0c75306b8a50f
Markus John <johm@quyo.de>
2015-05-25 08:32:57 +0200

    Add Iterator tests

--- test/DataStructures/Iterator-test.jl
+++ test/DataStructures/Iterator-test.jl
@@ -1,5 +1,19 @@
+using Brainstorm
+using Base.Test
+
+function test_iterator_takewhile()
+  @test collect(takewhile(1:10, x -> x^2 < 25)) == [1, 2, 3, 4]
+end
+
+function test_iterator_takeuntil()
+  @test collect(takeuntil(1:10, x -> x^2 >= 25)) == [1, 2, 3, 4, 5]
+end
 
 function test_iterator_all()
   print("DataStructures.Iterator...")
+
+  test_iterator_takewhile()
+  test_iterator_takeuntil()
+
   println("PASS")
 end
