3de8643540b7e43a7cfaca3bc2065043582091bc
Markus John <johm@quyo.de>
2015-06-07 15:33:12 +0200

    Include MinimalPerfectHashes package

--- src/Algorithms/Algorithms.jl
+++ src/Algorithms/Algorithms.jl
@@ -3,6 +3,7 @@
 using Reexport.@reexport
 
 VERSION < v"0.4-" && @reexport using IterationManagers
+#@reexport using MinimalPerfectHashes                     # doesn't work
 @reexport using SortingAlgorithms
 
 end
