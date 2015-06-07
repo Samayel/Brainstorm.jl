c8b7621fad14719deb7cfb0be8ae9f20e6a3e4f2
Markus John <johm@quyo.de>
2015-06-07 16:12:41 +0200

    Include FastAnonymous package

--- src/Algorithms/Algorithms.jl
+++ src/Algorithms/Algorithms.jl
@@ -2,6 +2,7 @@
 
 using Reexport.@reexport
 
+@reexport using FastAnonymous
 VERSION < v"0.4-" && @reexport using IterationManagers
 #@reexport using MinimalPerfectHashes                       # doesn't work
 @reexport using SortingAlgorithms
