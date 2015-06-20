af5df773dd5f3bfd080ccfc4f417eb83d03d5bdc
Markus John <johm@quyo.de>
2015-06-20 08:14:16 +0200

    Add testtimed() macro and includeall() function

--- src/Meta/Meta.jl
+++ src/Meta/Meta.jl
@@ -12,4 +12,39 @@ Base.map(f::Base.Callable, ex::Expr) =
     Expr(ex.head, [isa(e, Expr) ? map(f, e) : f(e) for e in ex.args]...)
 @reexport using ValueDispatch
 
+export @testtimed, includeall
+
+macro testtimed(label, func, tests)
+    quote
+        local llabel = $(esc(label))
+        print("$llabel... ")
+
+        local lfunc = $(esc(func))
+        precompile(lfunc, ())
+
+        local lstats1 = @timed lfunc()
+        local lstats2 = @timed lfunc()
+
+        $(esc(tests))
+
+        local lsolution = lstats1[1]
+        local lduration1 = @sprintf("%11.6f", lstats1[2])
+        local lduration2 = @sprintf("%11.6f", lstats2[2])
+        println("PASS   [$(lduration1)s / $(lduration2)s]   $lsolution")
+    end
+end
+
+function includeall(directory::String, skipFiles = true)
+    for entry in readdir(directory)
+        path = joinpath(directory, entry)
+
+        isdir(path) && includeall(path, false)
+        skipFiles && continue
+        !isfile(path) && continue
+        !Base.endswith(path, ".jl") && continue
+
+        include(path)
+    end
+end
+
 end
