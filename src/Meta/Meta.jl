30cdd2bfe7a9b2ce2990d1b3fa569a4c38b0b723
Markus John <johm@quyo.de>
2015-06-20 08:51:23 +0200

    Replace includeall() with get_jl_filenames()

--- src/Meta/Meta.jl
+++ src/Meta/Meta.jl
@@ -12,7 +12,9 @@ Base.map(f::Base.Callable, ex::Expr) =
     Expr(ex.head, [isa(e, Expr) ? map(f, e) : f(e) for e in ex.args]...)
 @reexport using ValueDispatch
 
-export @testtimed, includeall
+export
+    @testtimed,
+    get_jl_filenames
 
 macro testtimed(label, func, tests)
     quote
@@ -34,17 +36,19 @@ macro testtimed(label, func, tests)
     end
 end
 
-function includeall(directory::String, skipFiles = true)
+function get_jl_filenames(directory::String, skipRootFiles = false)
+    files = String[]
     for entry in readdir(directory)
         path = joinpath(directory, entry)
 
-        isdir(path) && includeall(path, false)
-        skipFiles && continue
+        isdir(path) && push!(files, get_jl_filenames(path, false)...)
+        skipRootFiles && continue
         !isfile(path) && continue
         !Base.endswith(path, ".jl") && continue
 
-        include(path)
+        push!(files, path)
     end
+    files
 end
 
 end
