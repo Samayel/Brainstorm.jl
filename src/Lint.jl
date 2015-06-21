3798d45aa50548d920a77018b9440610dda35986
Markus John <johm@quyo.de>
2015-06-21 09:15:45 +0200

    Fix previous commit regarding Lint

--- src/Lint.jl
+++ src/Lint.jl
@@ -1,12 +1,10 @@
-using Lint
+macro lintpragma(s)
+end
 
-@lintpragma("Ignore undefined module Combinatorics")
 @lintpragma("Ignore undefined module DataStructures.SortedDict")
-@lintpragma("Ignore undefined module Formatting")
 @lintpragma("Ignore undefined module Iterators.drop")
 @lintpragma("Ignore undefined module Iterators.imap")
 @lintpragma("Ignore undefined module Iterators.take")
-@lintpragma("Ignore undefined module Lazy")
 @lintpragma("Ignore undefined module Lint.lintpkg")
 @lintpragma("Ignore undefined module Pipe.@pipe")
 @lintpragma("Ignore undefined module Reexport.@reexport")
@@ -16,4 +14,11 @@ using Lint
 @lintpragma("Ignore undefined module Brainstorm.Math.checked_sub")
 
 @lintpragma("Ignore use of undeclared variable _")
-@lintpragma("Ignore use of undeclared variable nothing") # v0.4 only
+
+if VERSION >= v"0.4-"
+    @lintpragma("Ignore undefined module Combinatorics")
+    @lintpragma("Ignore undefined module Formatting")
+    @lintpragma("Ignore undefined module Lazy")
+
+    @lintpragma("Ignore use of undeclared variable nothing")
+end
