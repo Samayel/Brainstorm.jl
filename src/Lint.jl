a8fd50851940b9df93fbe0a67c16879dfd103564
Markus John <johm@quyo.de>
2015-06-21 09:04:56 +0200

    Ignore some Lint errors in 0.4

--- src/Lint.jl
+++ src/Lint.jl
@@ -1,9 +1,12 @@
 using Lint
 
+@lintpragma("Ignore undefined module Combinatorics")
 @lintpragma("Ignore undefined module DataStructures.SortedDict")
+@lintpragma("Ignore undefined module Formatting")
 @lintpragma("Ignore undefined module Iterators.drop")
 @lintpragma("Ignore undefined module Iterators.imap")
 @lintpragma("Ignore undefined module Iterators.take")
+@lintpragma("Ignore undefined module Lazy")
 @lintpragma("Ignore undefined module Lint.lintpkg")
 @lintpragma("Ignore undefined module Pipe.@pipe")
 @lintpragma("Ignore undefined module Reexport.@reexport")
@@ -13,3 +16,4 @@ using Lint
 @lintpragma("Ignore undefined module Brainstorm.Math.checked_sub")
 
 @lintpragma("Ignore use of undeclared variable _")
+@lintpragma("Ignore use of undeclared variable nothing") # v0.4 only
