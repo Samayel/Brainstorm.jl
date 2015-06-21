2e398c4d147e788dbb64d4ecc436933b7209f40a
Markus John <johm@quyo.de>
2015-06-21 12:03:47 +0200

    Include more packages

--- src/Algorithms/Algorithms.jl
+++ src/Algorithms/Algorithms.jl
@@ -4,6 +4,7 @@ using Reexport.@reexport
 
 @reexport using FastAnonymous
 VERSION < v"0.4-" && @reexport using IterationManagers
+@reexport using JellyFish
 #@reexport using MinimalPerfectHashes                       # doesn't work
 @reexport using SortingAlgorithms
 
--- src/DataStructures/DataStructures.jl
+++ src/DataStructures/DataStructures.jl
@@ -3,13 +3,18 @@
 using Reexport.@reexport
 
 @reexport using AutoHashEquals
+@reexport using BloomFilters
 @reexport using DataStructures
 @reexport using FunctionalCollections
 #@reexport using Graphs                         # conflicts with LightGraphs
+@reexport using ImmutableArrays
+@reexport using IndexedArrays
 @reexport using Iterators
 VERSION < v"0.4-" && @reexport using Lazy
 @reexport using LightGraphs
+@reexport using NamedArrays
 @reexport using NamedTuples
+@reexport using ShowSet
 @reexport using SparseVectors
 
 include("Iterators.jl")
