e4f687d8aa51b89510335396ef0b5f578286feed
Markus John <johm@quyo.de>
2015-06-28 12:46:59 +0200

    Remove obsolete lintpragma

--- src/lint.jl
+++ src/lint.jl
@@ -25,7 +25,6 @@ if VERSION < v"0.4-"
 else
     @lintpragma("Ignore undefined module Combinatorics")
     @lintpragma("Ignore undefined module Formatting")
-    @lintpragma("Ignore undefined module IterationManagers")
     @lintpragma("Ignore undefined module Lazy")
     @lintpragma("Ignore undefined module NamedArrays")
 
