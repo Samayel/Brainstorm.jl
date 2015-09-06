9428dcb07a7b3e6fc7336118c46b8cbb28c839d5
Markus John <johm@quyo.de>
2015-09-06 09:46:26 +0000

    Switch to v0.4-only

--- src/lint.jl
+++ src/lint.jl
@@ -1,13 +1,17 @@
 macro lintpragma(s)
 end
 
+@lintpragma("Ignore undefined module Combinatorics")
 @lintpragma("Ignore undefined module DataStructures.SortedDict")
+@lintpragma("Ignore undefined module Formatting")
 @lintpragma("Ignore undefined module Iterators.drop")
 @lintpragma("Ignore undefined module Iterators.imap")
 @lintpragma("Ignore undefined module Iterators.take")
+@lintpragma("Ignore undefined module Lazy")
 @lintpragma("Ignore undefined module Lint.lintpkg")
 @lintpragma("Ignore undefined module Lists.List")
 @lintpragma("Ignore undefined module Lists.ListNode")
+@lintpragma("Ignore undefined module NamedArrays")
 @lintpragma("Ignore undefined module TaylorSeries.Taylor1")
 @lintpragma("Ignore undefined module TaylorSeries.get_coeff")
 @lintpragma("Ignore undefined module TaylorSeries.taylor1_variable")
@@ -24,15 +28,5 @@ end
 @lintpragma("Ignore undefined module Brainstorm.@anon")
 
 @lintpragma("Ignore use of undeclared variable _")
-
-if VERSION < v"0.4-"
-    @lintpragma("Ignore undefined module Base.Dates")
-else
-    @lintpragma("Ignore undefined module Combinatorics")
-    @lintpragma("Ignore undefined module Formatting")
-    @lintpragma("Ignore undefined module Lazy")
-    @lintpragma("Ignore undefined module NamedArrays")
-
-    @lintpragma("Ignore use of undeclared variable nothing")
-    @lintpragma("Ignore use of undeclared variable :")
-end
+@lintpragma("Ignore use of undeclared variable nothing")
+@lintpragma("Ignore use of undeclared variable :")
