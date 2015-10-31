d5eb641bd7fa86eea7ffb85de61ec5da1ac4f887
Markus John <johm@quyo.de>
2015-10-31 09:03:09 +0100

    Remove more unused packages

--- deps/build.jl
+++ deps/build.jl
@@ -1,12 +1,5 @@
 
 try
-    eval(Expr(:import,:DeepConvert))
-catch err
-    Pkg.clone("https://github.com/jlapeyre/DeepConvert.jl")
-    Pkg.build("DeepConvert")
-end
-
-try
     eval(Expr(:import,:Lists))
 catch err
     Pkg.clone("https://github.com/Samayel/Lists.jl")
