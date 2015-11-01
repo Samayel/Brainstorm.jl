f177cf23c491f48093a5304533bd2c5240d4e5cd
Markus John <johm@quyo.de>
2015-11-01 11:35:32 +0100

    Refactor functors

--- src/Meta/functor.jl
+++ src/Meta/functor.jl
@@ -1,44 +1,43 @@
 @reexport module Functor
 
+import Base: call
 
 #= Meta =#
 
-immutable IsA{T} <: Base.Func{1}
-end
-isa(t::Type) = IsA{t}()
-Base.call{T}(::IsA{T}, x) = Base.isa(x, T)
+immutable IsAFun{T} <: Base.Func{1} end
+isa(t::Type) = IsAFun{t}()
+call{T}(::IsAFun{T}, x) = Base.isa(x, T)
 
 
 #= Collections =#
 
-immutable GetIndex{T} <: Base.Func{1}
+immutable GetIndexFun{T} <: Base.Func{1}
     idx::T
 end
-idx(i) = GetIndex(i)
-Base.call(f::GetIndex, a) = a[f.idx]
+idx(i) = GetIndexFun(i)
+call(f::GetIndexFun, a) = a[f.idx]
 
 
 #= Comparison =#
 
-immutable Leq1{T} <: Base.Func{1}
+immutable Leq1Fun{T} <: Base.Func{1}
     b::T
 end
-leq(b) = Leq1(b)
-Base.call(f::Leq1, a) = a <= f.b
+leq(b) = Leq1Fun(b)
+call(f::Leq1Fun, a) = a <= f.b
 
-immutable Geq1{T} <: Base.Func{1}
+immutable Geq1Fun{T} <: Base.Func{1}
     b::T
 end
-geq(b) = geq1(b)
-Base.call(f::Geq1, a) = a >= f.b
+geq(b) = Geq1Fun(b)
+call(f::Geq1Fun, a) = a >= f.b
 
 
 #= Math =#
 
-immutable Trunc{T} <: Base.Func{1}
-end
-trunc(t::Type) = Trunc{t}()
-Base.call{T}(::Trunc{T}, x) = Base.trunc(T, x)
+immutable TruncFun{T} <: Base.Func{1} end
+trunc(t::Type) = TruncFun{t}()
+call{T}(::TruncFun{T}, x) = Base.trunc(T, x)
 
 
 end
