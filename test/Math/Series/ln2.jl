ffbbb14fb97ee917fc45ae659b958f8da0c50798
Markus John <johm@quyo.de>
2015-11-22 13:47:46 +0100

    Unify ln(2) tests

--- test/Math/Series/ln2.jl
+++ test/Math/Series/ln2.jl
@@ -20,21 +20,21 @@ function test_ln2_all()
     with_bigfloat_precision(400_000) do
         expected = [trunc(BigInt, log(big(2))*(big(10)^d)) for d in (1, 10, 100, 1_000, 10_000, 100_000)]
 
-        @test all([ln2_N2a(d) for d in (1, 10, 100, 1_000, 10_000)]          - expected[1:5] .== 0)
-        @test all([ln2_N2b(d) for d in (1, 10, 100, 1_000, 10_000)]          - expected[1:5] .== 0)
-        @test all([ln2_N2c(d) for d in (1, 10, 100, 1_000, 10_000)]          - expected[1:5] .== 0)
+        @test all([ln2_N2a(d) for d in (1, 10, 100, 1_000, 10_000, 100_000)] - expected .== 0)
+        @test all([ln2_N2b(d) for d in (1, 10, 100, 1_000, 10_000, 100_000)] - expected .== 0)
+        @test all([ln2_N2c(d) for d in (1, 10, 100, 1_000, 10_000, 100_000)] - expected .== 0)
 
-        @test all([ln2_N3a(d) for d in (1, 10, 100, 1_000, 10_000)]          - expected[1:5] .== 0)
-        @test all([ln2_N3b(d) for d in (1, 10, 100, 1_000, 10_000)]          - expected[1:5] .== 0)
-        @test all([ln2_N3c(d) for d in (1, 10, 100, 1_000, 10_000)]          - expected[1:5] .== 0)
-        @test all([ln2_N3d(d) for d in (1, 10, 100, 1_000, 10_000, 100_000)] - expected      .== 0)
-        @test all([ln2_N3e(d) for d in (1, 10, 100, 1_000, 10_000)]          - expected[1:5] .== 0)
+        @test all([ln2_N3a(d) for d in (1, 10, 100, 1_000, 10_000, 100_000)] - expected .== 0)
+        @test all([ln2_N3b(d) for d in (1, 10, 100, 1_000, 10_000, 100_000)] - expected .== 0)
+        @test all([ln2_N3c(d) for d in (1, 10, 100, 1_000, 10_000, 100_000)] - expected .== 0)
+        @test all([ln2_N3d(d) for d in (1, 10, 100, 1_000, 10_000, 100_000)] - expected .== 0)
+        @test all([ln2_N3e(d) for d in (1, 10, 100, 1_000, 10_000, 100_000)] - expected .== 0)
 
-        @test all([ln2_N4a(d) for d in (1, 10, 100, 1_000, 10_000)]          - expected[1:5] .== 0)
-        @test all([ln2_N4b(d) for d in (1, 10, 100, 1_000, 10_000)]          - expected[1:5] .== 0)
-        @test all([ln2_N4c(d) for d in (1, 10, 100, 1_000, 10_000)]          - expected[1:5] .== 0)
+        @test all([ln2_N4a(d) for d in (1, 10, 100, 1_000, 10_000, 100_000)] - expected .== 0)
+        @test all([ln2_N4b(d) for d in (1, 10, 100, 1_000, 10_000, 100_000)] - expected .== 0)
+        @test all([ln2_N4c(d) for d in (1, 10, 100, 1_000, 10_000, 100_000)] - expected .== 0)
 
-        @test all([ln2_N5a(d) for d in (1, 10, 100, 1_000, 10_000)]          - expected[1:5] .== 0)
+        @test all([ln2_N5a(d) for d in (1, 10, 100, 1_000, 10_000, 100_000)] - expected .== 0)
     end
 
     println("PASS")
