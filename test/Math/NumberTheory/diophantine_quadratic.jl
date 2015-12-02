e039cb7d92382adaeabc73bcc2ff3eef3de759d9
Markus John <johm@quyo.de>
2015-12-02 14:26:12 +0100

    Fix undefined order of diophantine solutions

--- test/Math/NumberTheory/diophantine_quadratic.jl
+++ test/Math/NumberTheory/diophantine_quadratic.jl
@@ -3,46 +3,56 @@ function test_diophantine_solve_diophantine_quadratic()
 
     # linear
     sol = solve(diophantine_equation_quadratic_xy(cx=3, c0=-9))
-    @test sol == [diophantine_onex_anyy(3)]
+    @test Set(sol) == Set([diophantine_onex_anyy(3)])
+    @test length(sol) == 1
     @test typeof(sol) == soltype
 
     # simple hyperbolic
     sol = solve(diophantine_equation_quadratic_xy(cxy=4, cx=2, cy=8, c0=4))
-    @test sol == [diophantine_onex_anyy(-2)]
+    @test Set(sol) == Set([diophantine_onex_anyy(-2)])
+    @test length(sol) == 1
     @test typeof(sol) == soltype
 
     sol = solve(diophantine_equation_quadratic_xy(cxy=4, cx=12, cy=8, c0=24))
-    @test sol == [diophantine_onex_anyy(-2), diophantine_anyx_oney(-3)]
+    @test Set(sol) == Set([diophantine_onex_anyy(-2), diophantine_anyx_oney(-3)])
+    @test length(sol) == 2
     @test typeof(sol) == soltype
 
     sol = solve(diophantine_equation_quadratic_xy(cxy=2, cx=5, cy=56, c0=7))
-    @test sol == [diophantine_solutions((-27, 64), (-29, -69), (-21, 7), (-35, -12), (-9, 1), (-47, -6), (105, -2), (-161, -3))]
+    @test Set(sol) == Set([diophantine_solutions((-27, 64), (-29, -69), (-21, 7), (-35, -12), (-9, 1), (-47, -6), (105, -2), (-161, -3))])
+    @test length(sol) == 1
     @test typeof(sol) == soltype
 
     # elliptical
     sol = solve(diophantine_equation_quadratic_xy(cx²=42, cxy=8, cy²=15, cx=23, cy=17, c0=-4915))
-    @test sol == [diophantine_solutions((-11, -1))]
+    @test Set(sol) == Set([diophantine_solutions((-11, -1))])
+    @test length(sol) == 1
     @test typeof(sol) == soltype
 
     # parabolic
     sol = solve(diophantine_equation_quadratic_xy(cx²=1, cxy=2, cy²=1, cx=2, cy=2, c0=1))
-    @test sol == [diophantine_linearx_lineary(1, 0, -1, -1)]
+    @test Set(sol) == Set([diophantine_linearx_lineary(1, 0, -1, -1)])
+    @test length(sol) == 1
     @test typeof(sol) == soltype
 
     sol = solve(diophantine_equation_quadratic_xy(cx²=1, cxy=2, cy²=1, cx=6, cy=6, c0=5))
-    @test sol == [diophantine_linearx_lineary(1, 0, -1, -1), diophantine_linearx_lineary(1, 0, -1, -5)]
+    @test Set(sol) == Set([diophantine_linearx_lineary(1, 0, -1, -1), diophantine_linearx_lineary(1, 0, -1, -5)])
+    @test length(sol) == 2
     @test typeof(sol) == soltype
 
     sol = solve(diophantine_equation_quadratic_xy(cx²=8, cxy=-24, cy²=18, cx=5, cy=7, c0=16))
-    @test sol == [diophantine_quadraticx_quadraticy(-174, 17, -2, -116, 21, -2), diophantine_quadraticx_quadraticy(-174, 41, -4, -116, 37, -4)]
+    @test Set(sol) == Set([diophantine_quadraticx_quadraticy(-174, 17, -2, -116, 21, -2), diophantine_quadraticx_quadraticy(-174, 41, -4, -116, 37, -4)])
+    @test length(sol) == 2
     @test typeof(sol) == soltype
 
     # hyperbolic (WIP)
     sol = solve(diophantine_equation_quadratic_xy(cx²=1, cxy=3, cy²=2))
-    @test sol == [diophantine_solutions((0, 0)), diophantine_linearx_lineary(2, 0, -1, 0), diophantine_linearx_lineary(1, 0, -1, 0)]
+    @test Set(sol) == Set([diophantine_solutions((0, 0)), diophantine_linearx_lineary(2, 0, -1, 0), diophantine_linearx_lineary(1, 0, -1, 0)])
+    @test length(sol) == 3
     @test typeof(sol) == soltype
 
     sol = solve(diophantine_equation_quadratic_xy(cx²=1, cxy=3, cy²=2, c0=10))
-    @test sol == [diophantine_solutions((-21, 11), (21, -11), (-12, 7), (12, -7), (-9, 7), (9, -7), (-12, 11), (12, -11))]
+    @test Set(sol) == Set([diophantine_solutions((-21, 11), (21, -11), (-12, 7), (12, -7), (-9, 7), (9, -7), (-12, 11), (12, -11))])
+    @test length(sol) == 1
     @test typeof(sol) == soltype
 end
