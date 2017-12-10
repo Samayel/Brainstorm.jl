48277b3123675ebb4686623789f783a4e0133400
Markus John <johm@quyo.de>
2017-12-10 18:11:05 +0100

    Patch Nemo on-the-fly, make Travis and Appveyor happy

--- deps/build.jl
+++ deps/build.jl
@@ -12,3 +12,11 @@ catch err
     Pkg.clone("https://github.com/jlep/Multicombinations.jl")
     Pkg.build("Multicombinations")
 end
+
+
+file = joinpath(Pkg.dir("Nemo"), "src", "flint", "fmpq_rel_series.jl")
+content = readstring(file)
+content = replace(content, "fmpq_poly_scalar_divexact_", "fmpq_poly_scalar_div_")
+open(file, "w") do f
+    write(f, content)
+end
